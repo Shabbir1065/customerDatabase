e85546c07bf4bbe11b425152578eab5ae92064ea
Shabbir1065 <shabbir1065@gmail.com>
2023-01-05 23:20:29 -0500

    Update StudentConfig.java

--- src/main/java/com/example/customerDatabase/Customer/StudentConfig.java
+++ src/main/java/com/example/customerDatabase/Customer/StudentConfig.java
@@ -6,20 +6,40 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+    CommandLineRunner commandLineRunner(CustomerRepository repository){
         return args -> {
-            new Customer(
-                    1L,
+            Customer shabbir = new Customer(
                     "Shabbir",
                     "shabbir1065@gmail.com",
                     "4162223333",
                     LocalDate.of(2003, Month.OCTOBER,25),
                     "1234 Derry rd"
+            );
+
+            Customer jonathan = new Customer(
+                    "Jonathan",
+                    "jono.lasang@gmail.com",
+                    "4161115555",
+                    LocalDate.of(2003, Month.AUGUST,17),
+                    "5678 Trafalgar rd"
+            );
+
+            Customer taqi = new Customer(
+                    "Taqi",
+                    "taqi313@gmail.com",
+                    "6471213434",
+                    LocalDate.of(2003, Month.OCTOBER,19),
+                    "123 Ruhl rd"
+            );
+
+            repository.saveAll(
+                    List.of(shabbir, jonathan, taqi)
             );
         };
     }
